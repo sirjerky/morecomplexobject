9536a78f545d9abc40937be3aa253473cc320781
sirjerky <brownad78@gmail.com>
2014-09-01 14:18:15 -0700

    make changes to files for new commit

--- app/js/complex_object.js
+++ app/js/complex_object.js
@@ -1,10 +1,13 @@
 "use strict";
 
 module.exports = function Counter(){
+  // set counter to zero
   var counter = 0;
+  // return value of counter
   this.getValue = function(){
-    console.log(counter);
+    return counter;
   };
+  // increment counter
   this.increment = function(){
     counter++;
   };
